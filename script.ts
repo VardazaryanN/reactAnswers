75cb4cffc6f0c9dfd0c1edeb2938b5d98339030c
Natalia Vardazaryan <88931980+VardazaryanN@users.noreply.github.com>
2022-02-04 11:20:09 +0400

    Update script.ts

--- script.ts
+++ script.ts
@@ -1,14 +1,22 @@
 
 
-function test(num: number): any{
+function candy(num: number): string{
     if (num % 2 === 0){
         return "candy";
-    } else if (num % 11 === 0){
-        return "bar";
-    }else  if(num % 22 == 0){
-        return "candybar";
-    } else{
-        return num;
     }
 }
-test(654)
+function bar(num: number): string{
+   if (num % 11 === 0){
+        return "bar";
+    }
+}
+function candybar(num: number): string{
+   if(num % 22 == 0){
+        return "candybar";
+    }
+}
+function nmberValue(num: number): number{
+  
+        return num;
+    
+}
